3472cfe6f006df4bf4c05580d26d60cf923475cf
Nurlan Ibragimov <nibragimov@1cb.kz>
2023-02-01 09:37:04 +0600

    +fix

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
         <>
             <div className={"bg-birch p-6"}>
                 <div className={"container mx-auto flex justify-between items-center"}>
-                    <Link to={"/"} className={"text-xl font-bold text-purple-100 hover:text-purple-dark"}>REACT-EFFECTOR</Link>
+                    <Link to={"/"} className={"text-xl font-bold text-purple-100 hover:text-purple-dark"}>REACT-AXIOS-KY</Link>
                     <div className={"flex gap-8"}>
                         {links.map(link => (
                             <Link
